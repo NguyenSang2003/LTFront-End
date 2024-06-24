214557c93eae3a94be2a69ff7f51846b11234812
NguyenSang2003 <21130512@st.hcmuaf.edu.vn>
2024-06-24 07:31:18 +0700

    Khoi tao login & sendmessage

---
+++ src/utils/websocket.ts
@@ -0,0 +1,27 @@
+export const connectWebSocket = (onMessage: (msg: MessageEvent) => void): WebSocket => {
+    const socket = new WebSocket('ws://140.238.54.136:8080/chat/chat');
+
+    socket.onopen = () => {
+        console.log('WebSocket connected');
+    };
+
+    socket.onmessage = onMessage;
+
+    socket.onclose = () => {
+        console.log('WebSocket disconnected');
+    };
+
+    socket.onerror = (error) => {
+        console.error('WebSocket error: ', error);
+    };
+
+    return socket;
+};
+
+export const sendMessage = (socket: WebSocket, message: object) => {
+    if (socket.readyState === WebSocket.OPEN) {
+        socket.send(JSON.stringify(message));
+    } else {
+        console.error('WebSocket is not open. Ready state: ', socket.readyState);
+    }
+};
